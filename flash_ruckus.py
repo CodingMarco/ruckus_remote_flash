04d31ac955b5f84b872f054a5e5c87aa4f94ba8d
Marco von Rosenberg <codingmarco@gmail.com>
2025-03-30 23:23:15 +0200

    Successfully open a root shell

--- flash_ruckus.py
+++ flash_ruckus.py
@@ -1,24 +1,85 @@
+import time
+import logging
 import argparse
-from invoke import Responder
-from fabric import Connection
+import paramiko
+from custom_logging import CustomFormatter
+
+PROMPT_BUSYBOX_ROOT = "# "
+
+
+def setup_logging():
+    logger = logging.getLogger("flash_ruckus")
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(CustomFormatter())
+    logger.addHandler(ch)
+
+    return logger
+
+
+logger = setup_logging()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Flash Ruckus AP")
-    parser.add_argument("ip", type=str, help="IP Address of the AP")
     parser.add_argument(
-        "username", type=str, default="super", help="Username of the AP"
+        "--ip", type=str, default="192.168.0.1", help="IP Address of the AP"
     )
     parser.add_argument(
-        "password", type=str, default="sp-admin", help="Password of the AP"
+        "--username", type=str, default="super", help="Username of the AP"
     )
     parser.add_argument(
-        "firmware",
+        "--password", type=str, default="sp-admin", help="Password of the AP"
+    )
+    parser.add_argument(
+        "--firmware",
         type=str,
         default="openwrt-sysupgrade.bin",
         help="Firmware file to flash",
     )
     return parser.parse_args()
+
+
+def read_shell(shell) -> str:
+    while not shell.recv_ready():
+        time.sleep(0.1)
+
+    return shell.recv(4096).decode()
+
+
+def wait_for_prompt(shell, prompt) -> str:
+    buffer = ""
+    while True:
+        output = read_shell(shell)
+        logger.debug(output)
+        buffer += output
+
+        if prompt in output:
+            break
+
+    return buffer
+
+
+def respond_to_prompt(shell, prompt, response) -> None:
+    wait_for_prompt(shell, prompt)
+    shell.send(response)
+
+
+def send_command_wait(shell, command) -> str:
+    if not command.endswith("\n"):
+        command += "\n"
+
+    shell.send(command)
+    output = wait_for_prompt(shell, PROMPT_BUSYBOX_ROOT)
+    output = (
+        output.replace("\r\n", "\n")
+        .replace(PROMPT_BUSYBOX_ROOT, "")
+        .replace(command, "")
+        .strip()
+    )
+
+    return output
 
 
 def main():
@@ -28,14 +89,32 @@ def main():
     password = args.password
     firmware = args.firmware
 
-    conn = Connection(host=ip, user=username)
-
-    enter_username = Responder(pattern="Please login: ", response=f"{username}\n")
-    enter_password = Responder(pattern="password : ", response=f"{password}\n")
-    confirm_root_shell = Responder(pattern="What's your chow: ", response="\n")
-
     # Open the initial root shell
-    conn.run("!v54!", watchers=[enter_username, enter_password, confirm_root_shell])
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        ip,
+        username=username,
+        password=password,
+        allow_agent=False,
+        look_for_keys=False,
+    )
+
+    shell = client.invoke_shell()
+
+    # Respond to the initial prompts
+    respond_to_prompt(shell, "Please login: ", f"{username}\n")
+    respond_to_prompt(shell, "password : ", f"{password}\n")
+    respond_to_prompt(shell, "rkscli: ", "!v54!\n")
+    respond_to_prompt(shell, "What's your chow: ", "\n")
+    wait_for_prompt(shell, PROMPT_BUSYBOX_ROOT)
+
+    output = send_command_wait(shell, "pwd")  # output -> '/'
+    print(f"Output: '{output}'")
+
+    # Close everything
+    shell.close()
+    client.close()
 
 
 if __name__ == "__main__":
